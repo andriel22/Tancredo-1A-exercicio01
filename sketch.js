1ed0d931900ab4ad084ad4ec5de82aaef8137b4a
andriel22 <141413682+andriel22@users.noreply.github.com>
2023-08-11 08:18:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(200, 200);
+  background("silver");
+  
+}
+
+function draw() {
+  
+  
+  stroke("purple");
+  fill("gold");
+  
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,30,30);
+  }
+  
+}
